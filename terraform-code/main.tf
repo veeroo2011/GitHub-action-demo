d2b211e2cafbb11cc10837c2893a7496a31af544
Abhimnayu kumar <52759159+veeroo2011@users.noreply.github.com>
2026-08-20 01:16:47 +0530

    Deploy using kubernetes (#19)
    * add env
    
    * fix typo
    
    * add Debug
    
    * add Debug
    
    * add terraform code and workflow
    
    ---------
    
    Co-authored-by: Abhimanyu <abhimanyu.113065@gmail.com>

---
+++ terraform-code/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket = "github-actions-terraform-demo-997554581092"
+}
